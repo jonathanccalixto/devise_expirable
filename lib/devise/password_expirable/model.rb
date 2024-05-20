d06701a12c704a985a16e837516a7fa08d82102f
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2024-05-19 23:26:55 -0300

    add callback in model to update expiration_password_at

--- lib/devise/password_expirable/model.rb
+++ lib/devise/password_expirable/model.rb
@@ -5,6 +5,10 @@ module Devise
     module PasswordExpirable
       extend ActiveSupport::Concern
 
+      included do
+        before_save :change_expiration_password_at
+      end
+
       def password_expirate_in
         self.class.password_expirate_in
       end
@@ -27,7 +31,19 @@ module Devise
         password_expired? ? :password_expired : super
       end
 
+      protected
+
+      def next_password_expiration_time
+        password_expirate_in.from_now
+      end
+
       private
+
+      def change_expiration_password_at
+        return unless encrypted_password_changed?
+
+        self.expiration_password_at = next_password_expiration_time
+      end
 
       module ClassMethods
         Devise::Models.config(self, :password_expirate_in)
